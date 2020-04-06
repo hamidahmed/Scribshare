1f81a75b61074ce3d6b586d3816cad5c41626e27
ahmed1003 <thedraft101@gmail.com>
2020-04-05 22:45:26 -0400

    Additions

--- src/main/Layout.js
+++ src/main/Layout.js
@@ -2,14 +2,30 @@ import React from "react";
 
 import "./Layout.css";
 import LayoutNavigation from "./LayoutNavigation";
+import friend from "../friend.png";
+import documents from "../documents.png";
+
 const Layout = props => {
   return (
     <div>
-      <LayoutNavigation />
       <div class="app-layout">
-        <div class="teams">Groups</div>
-        <div class="channels">Channels</div>
-        <div class="users">Users</div>
+        <div class="teams"></div>
+        <div class="channels">
+          <div className="nav-btn">
+            <div class="container-1">
+              <button>
+                <img src={documents} alt="doc" className="doc-icon" />
+                Documents
+              </button>
+            </div>
+            <div class="container-1">
+              <button>
+                <img src={friend} alt="friend" className="friend-icon" />
+                Friends
+              </button>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
